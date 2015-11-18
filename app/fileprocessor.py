b41fb224beb9268c930bc4ff106c7990cfa1aae5
Jeremiah Marks <jeremmiah@jlmarks.org>
2015-11-18 08:13:55 -0700

    Stopping point from the night

--- app/fileprocessor.py
+++ app/fileprocessor.py
@@ -1,25 +1,28 @@
 #!/c/Users/jeremiah.marks/AppData/Local/Continuum/Anaconda2/python
+# So I am have not tested it yet, but this should be more or less what we need.
+# Well, mostly. Basically, don't use this yet, but it is almost there, I think.
+
 #####################################
-# This file exists solely to house 
-# one point of logic, matching the 
-# data between the files. 
+# This file exists solely to house
+# one point of logic, matching the
+# data between the files.
 # it will use the following bits of data:
     # location of Account.csv
     # location of Attachment.csv
-    # location of a recent export of the 
-        # Infusionsoft company records, to 
+    # location of a recent export of the
+        # Infusionsoft company records, to
         # include the company name, the company
         # id, the main contact first/last name
-    # some form of api connection with a 
+    # some form of api connection with a
         # getallrecords method
         # .connection.FileService.uploadFile method
-    # location of the folder that contains all of the 
-    # files that need to be uploaded. 
-# 
-# 
-# 
-# 
-# 
+    # location of the folder that contains all of the
+    # files that need to be uploaded.
+#
+#
+#
+#
+#
 #####################################
 import csv
 import os
@@ -35,26 +38,42 @@ class FileLogician(object):
     FileLogician will handle various methods
     needed to match the data as needed.
     Some methods that it will include, though,
-    will be the ability to get the first or all 
+    will be the ability to get the first or all
     rows that match criteria.
     Additionally, it will probably implement a v-lookup procedure.
         Like seriously, why have I not done that yet?
     """
-    def __init__(self, apiConnection=None):
+    def __init__(self, apiConnection=None, logger=None):
         super(FileLogician, self).__init__()
         self.svr = apiConnection
         self.acctidToContact={}
         self.Accountpath = None
         self.AttachmentCSVpath = None
         self.exportpath = None
+        self.logger=logger
+        self.uploadedcsvfile='uploaded.csv'
+
+    def logdata(self, stringtowriteout):
+        if logger is None:
+            pass
+        else:
+            self.logger(stringtowriteout)
+
+    def writecsvfile(self, dicttoupload):
+        with open(self.uploadedcsvfile, 'a+') as outfile:
+            outfile.write(','.join([str(dicttoupload[x]) for x in sorted(dicttoupload.keys())]))
 
     def setAccount(self, newAccount):
         self.Accountpath=newAccount
+        self.logdata("FileLogician Set account to " + str(newAccount))
     def setAttachment(self, newAttachment):
+        self.logdata("FileLogician Set Attachment.csv to " + str(newAttachment))
         self.AttachmentCSVpath=newAttachment
     def setexport(self, newexport):
+        self.logdata("FileLogician Set company export.csv to " + str(newexport))
         self.exportpath=newexport
     def setfilefolder(self, pathtofolder):
+        self.logdata("FileLogician Set path to the files folder to " + str(pathtofolder))
         self.filefolder = pathtofolder
 
     def startToProcess(self):
@@ -73,15 +92,34 @@ class FileLogician(object):
                         thisfileupload['filename'] = eachrow['Name']
                         searchcriteria={}
                         searchcriteria['Id'] = thisfileupload['AccountId']
-                        thisfileupload['companyName'] = thisAccountFile.getlineswith(searchcriteria)['Name']
+                        companyreturns = thisAccountFile.getlineswith(searchcriteria)
+
+                        if type(companyreturns) == type(list()):
+                            # This will test if a list was returned.  If a list was returned
+                            # then there were no results found. (In this implementation,
+                            # that method could return a list if "First" is not selected)
+                            self.logdata("FileLogician was unable to find the company with ID = " + str(thisfileupload['AccountId'] +
+                                                " in the Account.csv file"))
+                            continue
+
+                        thisfileupload['companyName'] = companyreturns['Name']
                         thisfileupload['Contact'] = {}
 
                         searchcriteria = {}
                         searchcriteria['Company'] = thisfileupload['companyName']
                         matchingrow = thisexportfile.getlineswith(searchcriteria)
+                        if len(matchingrow) == 0:
+                            # Basically this is here for the same reason I check to see if
+                            # the list came back empty for the same reasons as above.
+                            self.logdata("FileLogician was unable to find the company named " + str(searchcriteria['Company']) +
+                                                " in the export from Infusionsoft.")
+                            continue
+
+
                         thisfileupload['Contact']['FirstName']=matchingrow['Main Contact First Name']
                         thisfileupload['Contact']['LastName'] = matchingrow['Main Contact Last Name']
                         thisfileupload['Contact']['CompanyId'] = int(matchingrow['Id'])
+                        self.logdata("FileLogician is about to do a search for " + str(thisfileupload['Contact']))
 
                         allmatching = self.svr.getallrecords('Contact', searchcriteria=searchcriteria)
                         if len(allmatching) == 0:
@@ -89,13 +127,19 @@ class FileLogician(object):
                         else:
                             thisfileupload['conid'] = allmatching[0]['Id']
                         self.acctidToContact[eachrow['AccountId']] = thisfileupload['conid']
-                    self.uploadfile(self.acctidToContact[eachrow['AccountId']], 
+                        self.logdata("FileLogician matched these values " + str(thisfileupload))
+                    self.uploadfile(self.acctidToContact[eachrow['AccountId']],
                         eachrow['Name'],
                         os.path.abspath(os.path.join(self.filefolder, eachrow['Id'])))
 
     def uploadfile(self, contactid, filename, filepath):
-        print contactid
-        print self.svr.connection.FileService.uploadFile(self.svr.infusionsoftapikey, contactid, filename, base64.b64encode(open(filepath, 'rb').read()))
+        # print contactid
+        thisfileid = self.svr.connection.FileService.uploadFile(self.svr.infusionsoftapikey, contactid, filename, base64.b64encode(open(filepath, 'rb').read()))
+        logstr = "FileLogician uploaded file %s to contact id %s . " %(filename, str(contactid))
+        self.logdata(logstr)
+        print logstr
+        self.writecsvfile({'contactid': contactid, 'filename': filename, 'filepath': filepath})
+
 
 class FileActions(object):
     """docstring for FileActions
@@ -133,22 +177,22 @@ class CSVFileActions(FileActions):
         with open(self.fpath, 'rb') as infile:
             thisreader = csv.DictReader(infile)
 
-    def getlineswith(self, 
+    def getlineswith(self,
                      searchcriteria,
                      first=True,
                      sorting=False,
                      desiredColumns=False):
         """getlineswith accepts a dict of
         columnName->value pairs to search for.
-        If first is True and sorting is none 
+        If first is True and sorting is none
         it will stop the first time it finds
-        a matching row and return it. sorting 
-        will be done via whatever method is 
-        passed to sorting. Basically, it will 
+        a matching row and return it. sorting
+        will be done via whatever method is
+        passed to sorting. Basically, it will
         need to accept two dictionaries (rows
         in the csv file) and return whichever
         is the one that is now in "first".
-        If first is false, the method will 
+        If first is false, the method will
         return all rows that match in a list.
 
         edit to add: desired columns is there
@@ -192,6 +236,6 @@ class CSVFileActions(FileActions):
                     return returnlist
 
 
-        
+
 
 
