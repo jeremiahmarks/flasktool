bde322c3ee8435f6b1dd0144bb43b9d3f5e007f5
Jeremiah Marks <jeremmiah@jlmarks.org>
2015-11-18 19:27:46 -0700

    Remove legacy code from original tutorial.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'q^TAxJvQuUEj&JbMjpj6&!byXJk88RpqxUWNR8kD$BKc4X!sz*DZubTJ@fUj&$SkmzS0Edecsi*mcrBvvxGnLL1gh^cMcdit0iM8'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
 
     @staticmethod
     def init_app(app):
